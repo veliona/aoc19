6ca672e33778e564cbd156979812215ee63c61a8
veliona <jakubczak.urszula.julia@gmail.com>
2019-12-14 10:11:25 +0100

    Update the solution for part 1

--- Day04/solution_part1.py
+++ Day04/solution_part1.py
@@ -1,28 +1,23 @@
 def is_increasing_digits_sequence(number):
-    for number in numbers:
-        sequence = list(map(int,str(number)))
-        print(sequence)
-        for sequence in number:
-            x = 0
-            results = 0
-            for value in sequence:
-                y = value
-                if (y > x and y=x):
-                    results +=
-    print(results)
-        #value = 0
-        #for sequence in :
-        #    value += seq_value
+    sequence = list(map(int,str(number)))
+    print(sequence)
+    for n in sequence:
+        i=0
+        for i in range(len(n)):
+            if n[i+1] > n[i]:
+                print(n)
 
 
-
-
-
-
-
-
-
-
+with open(f"data.txt") as f:
+    content = f.readlines()
+    numbers = [x.strip() for x in content][0].split("-")
+    numbers = [int(x) for x in numbers]
+    list_numbers = list(range(numbers[0], numbers[1]))
+    for number in list_numbers:
+        if is_increasing_digits_sequence(number):
+            #TODO: Check if there is the same number
+            
+    print(number)
 
 
 #function is_increasing_digits_Sequence(num) {
@@ -40,12 +35,5 @@ def is_increasing_digits_sequence(number):
 
 
 
-with open(f"data.txt") as f:
-    content = f.readlines()
-    numbers = [x.strip() for x in content][0].split("-")
-    numbers = [int(x) for x in numbers]
-    list_numbers = list(range(numbers[0], numbers[1]))
-    print(is_increasing_digits_sequence(list_numbers[0]))
-    #for number in list_numbers:
-    #    if
+
 
