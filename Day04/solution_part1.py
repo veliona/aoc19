7276c3dc2f92163c389e9b705e423ea0626c2f92
veliona <jakubczak.urszula.julia@gmail.com>
2019-12-22 19:36:48 +0100

    Improve the code by adding two functions

--- Day04/solution_part1.py
+++ Day04/solution_part1.py
@@ -1,34 +1,38 @@
-def is_increasing_digits_sequence(list_numbers):
-    increasing_list = []
-    increasing_list_equal = []
-    for number in list_numbers:
-        sequence = list(map(int, str(number)))
-        i = 0
-        for n in sequence:
-            n = int(n)
-            for i in range(0, 4):
-                i += 1
-                if int(sequence[i+1]) < int(sequence[i]):
-                    return False
-                else:
-                    return True
-        increasing_list.append(number)
-    print(increasing_list)
-    for increasing_number in increasing_list:
-        increasing_sequence = list(map(int, str(increasing_number)))
-        i = 0
-        for increasing_n in increasing_sequence:
-            increasing_n = int(increasing_n)
-            for i in range(len(increasing_sequence) - 2):
-                i += 1
-                if int(increasing_sequence[i]) == int(increasing_sequence[i+1]):
-                    increasing_list_equal.append(increasing_number)
-    print(len(increasing_list_equal))
+def is_increasing_digits_sequence(number):
+    sequence = list(map(int, str(number)))
+    i = 0
+    for n in sequence:
+        n = int(n)
+        for i in range(0, 4):
+            i += 1
+            if int(sequence[i+1]) < int(sequence[i]):
+                return False
+            else:
+                return True
+
+
+def is_equal_digits(number):
+    sequence = list(map(int, str(number)))
+    i = 0
+    for n in sequence:
+        n = int(n)
+        for i in range(0, 4):
+            i += 1
+            if int(sequence[i + 1]) == int(sequence[i]):
+                return True
+            else:
+                return False
 
 
 with open(f"data.txt") as f:
     content = f.readlines()
     numbers = [x.strip() for x in content][0].split("-")
     numbers = [int(x) for x in numbers]
-    list_numbers = list(range(numbers[0], numbers[1]))
-    print(is_increasing_digits_sequence(list_numbers))
+    list_numbers = list(range(numbers[0], numbers[1] + 1))
+    print(len(list_numbers))
+    result_n = 0
+    for number in list_numbers:
+        if is_increasing_digits_sequence(number) is True & is_equal_digits(number) is True:
+            result_n += 1
+    print(result_n)
+
