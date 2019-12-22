28ab9c9355b72e4bf00f227f48e8c900826e4853
veliona <jakubczak.urszula.julia@gmail.com>
2019-12-22 17:19:42 +0100

    Update the solution for part 1

--- Day04/solution_part1.py
+++ Day04/solution_part1.py
@@ -1,11 +1,29 @@
-def is_increasing_digits_sequence(number):
-    sequence = list(map(int,str(number)))
-    print(sequence)
-    for n in sequence:
-        i=0
-        for i in range(len(n)):
-            if n[i+1] > n[i]:
-                print(n)
+def is_increasing_digits_sequence(list_numbers):
+    increasing_list = []
+    increasing_list_equal = []
+    for number in list_numbers:
+        sequence = list(map(int, str(number)))
+        i = 0
+        for n in sequence:
+            n = int(n)
+            for i in range(0, 4):
+                i += 1
+                if int(sequence[i+1]) < int(sequence[i]):
+                    return False
+                else:
+                    return True
+        increasing_list.append(number)
+    print(increasing_list)
+    for increasing_number in increasing_list:
+        increasing_sequence = list(map(int, str(increasing_number)))
+        i = 0
+        for increasing_n in increasing_sequence:
+            increasing_n = int(increasing_n)
+            for i in range(len(increasing_sequence) - 2):
+                i += 1
+                if int(increasing_sequence[i]) == int(increasing_sequence[i+1]):
+                    increasing_list_equal.append(increasing_number)
+    print(len(increasing_list_equal))
 
 
 with open(f"data.txt") as f:
@@ -13,27 +31,4 @@ with open(f"data.txt") as f:
     numbers = [x.strip() for x in content][0].split("-")
     numbers = [int(x) for x in numbers]
     list_numbers = list(range(numbers[0], numbers[1]))
-    for number in list_numbers:
-        if is_increasing_digits_sequence(number):
-            #TODO: Check if there is the same number
-            
-    print(number)
-
-
-#function is_increasing_digits_Sequence(num) {
-
-#  var arr_num = ('' + num).split('');
-
-#  for (var i = 0; i < arr_num.length - 1; i++) {
-#    if (parseInt(arr_num[i]) >= parseInt(arr_num[i + 1]))
-#      return false;
-#    }
-#  return true;
-#}
-
-
-
-
-
-
-
+    print(is_increasing_digits_sequence(list_numbers))
